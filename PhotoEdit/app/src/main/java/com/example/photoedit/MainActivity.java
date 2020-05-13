da8309f3e0255433cf19c59b09adbc47b8a7325c
Octavianus Giovanni Y <octavianusgiovanni@gmail.com>
2020-05-13 14:57:31 +0700

    Update

--- PhotoEdit/app/src/main/java/com/example/photoedit/MainActivity.java
+++ PhotoEdit/app/src/main/java/com/example/photoedit/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.photoedit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final int REQUEST_PERMISSIONS = 1234;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private static final int PERMISSION_COUNT = 2;
+
+    @SuppressLint("NewApi")
+    private boolean notPermissions(){
+        for(int i=0; i<PERMISSION_COUNT; i++){
+            if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && notPermissions()){
+                requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS && grantResults.length > 0){
+            if(notPermissions()){
+                ((ActivityManager) this.getSystemService(ACTIVITY_SERVICE)).clearApplicationUserData();
+                recreate();
+            }
+        }
+    }
+
+    private void init(){
+//        if(MainActivity.this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+//            findViewById(R.id.takePhotoButton).setVisibility(View.GONE);
+//        }
+        final Button selectImageButton = findViewById(R.id.selectImageButton);
+
+        selectImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+
+        final Button takePhotoButton = findViewById(R.id.takePhotoButton);
+
+        takePhotoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
     }
 }
